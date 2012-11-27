76e260f6b0afcdfa82a6388cf74306b60308634b
Apprentice <apprentice@devbootcamp.com>
2012-11-26 17:20:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Manatee
+
+Get matched with mentors and mentor others!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manatee::Application.config.secret_token = 'c1b254a7ab6ea826223c5e0643a408ee759909929c9c485f68cde37b8491019ddb286ac338fe1a69b7ab09cfe8e66c6f681dfa72dc0e2997ca9a5d3b6134f8a3'
