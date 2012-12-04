3cb8a3ea6f040764ad76ed328dcaf78c1679d468
Apprentice <apprentice@devbootcamp.com>
2012-12-03 19:30:02 -0800

    update domain and host to match our heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,9 @@ Manatee::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
-
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.default_url_options = { :host => "apprenticehour.com" }
+  config.action_mailer.default_url_options = { :host => "floating-mountain-1779.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -77,7 +75,7 @@ Manatee::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "apprenticehour.com",
+    domain: "floating-mountain-1779.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
