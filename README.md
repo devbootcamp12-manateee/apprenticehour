f9358ab2dadaa969974316ce7cef526c755bcb4d
Daniel Gamboa <danielgamboa10@gmail.com>
2012-12-06 11:50:46 -0800

    update readme file

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Manatee
+# ApprenticeHour
 
-Get matched with mentors and mentor others!
+## Get matched with mentors and mentor others!
+
+Rails application built as a final project by Dev Bootcamp students,
+
+- [Kari Weiler](http://kariweiler.com)
+- [Kelsey Schlarman](http://github.com/kschlarman)
+- [Janet Lai Chang](http://janetchang.com)
+- [David Ragone](http://www.dmragone.com)
+- [Daniel Gamboa](http://github.com/dgamboa)
+
+![Screenshot](http://dgamboa.com/dbc/apprenticehour.png)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,12 +19,12 @@ describe User do
 	# how/do we valid association with meeting (via mentee and mentor)?
 	it { should have_many :mentor_meetings }
 	it { should have_many :mentee_meetings }
-	
+
 	it { should validate_presence_of :uid }
 	it { should validate_presence_of :email }
 	it { should validate_presence_of :provider }
 	it { should validate_presence_of :gravatar }
-	
+
 	it { should validate_uniqueness_of :email }
 
 	it { should_not allow_mass_assignment_of :uid }
@@ -33,7 +33,7 @@ describe User do
 	it { should_not allow_mass_assignment_of :gravatar }
 
 	describe 'remember_token' do
-		it 'exists' do
+		it 'is created on save' do
 			user = create(:user)
 			user.remember_token.should_not be_blank
 		end
@@ -54,18 +54,18 @@ describe User do
 			user = FactoryGirl.create(:user)
 			mentor_meeting = FactoryGirl.create(:matched_meeting, :mentor => user)
 			other_meeting = FactoryGirl.create(:meeting)
-			
+
 			user.mentor_meetings.should eq [mentor_meeting]
 		end
 	end
 
 	describe '.from_oauth' do
-		let(:fakeauth) { Hashie::Mash.new(:provider => 'github',
-				                              :uid => '12345',
-				                              :info => Hashie::Mash.new(:name => 'Bob',
-				                              	                        :email => 'bob@bob.com',
-				                              	                        :image => 'sasdad'),
-				                              :credentials => Hashie::Mash.new(:token => '1234',
+		let(:fakeauth) { Hashie::Mash.new(:provider    => 'github',
+				                              :uid         => '12345',
+				                              :info        => Hashie::Mash.new(:name  => 'Bob',
+				                              	                               :email => 'bob@bob.com',
+				                              	                               :image => 'sasdad'),
+				                              :credentials => Hashie::Mash.new(:token      => '1234',
 																				                               :expires_at => Time.now)) }
 
 		it 'creates a new user from github oauth information' do
